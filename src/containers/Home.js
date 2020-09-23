a5fa8656f8a844b579e5e6010cacebea1ab2c8fe
sdgupta123 <sdgupta@akvagroup.com>
2020-09-23 14:20:28 +0200

    Setting up the easy help application

---
+++ src/containers/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Home.css";
+
+export default function Home() {
+  return (
+    <div className="Home">
+      <div className="lander">
+        <h1>Easy Help</h1>
+        <p>A simple help app in pandemic</p>
+      </div>
+    </div>
+  );
+}
--- src/index.js
+++ src/index.js
@@ -2,12 +2,37 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { BrowserRouter as Router } from 'react-router-dom';
 import * as serviceWorker from './serviceWorker';
+import { Amplify } from 'aws-amplify';
 
 ReactDOM.render(
-  <React.StrictMode>
+  Amplify.configure({
+    Auth: {
+      mandatorySignIn: true,
+      region: config.cognito.REGION,
+      userPoolId: config.cognito.USER_POOL_ID,
+      identityPoolId: config.cognito.IDENTITY_POOL_ID,
+      userPoolWebClientId: config.cognito.APP_CLIENT_ID
+    },
+    Storage: {
+      region: config.s3.REGION,
+      bucket: config.s3.BUCKET,
+      identityPoolId: config.cognito.IDENTITY_POOL_ID
+    },
+    API: {
+      endpoints: [
+        {
+          name: "users",
+          endpoint: config.apiGateway.URL,
+          region: config.apiGateway.REGION
+        },
+      ]
+    }
+  });
+  <Router>
     <App />
-  </React.StrictMode>,
+  </Router>,
   document.getElementById('root')
 );
 
