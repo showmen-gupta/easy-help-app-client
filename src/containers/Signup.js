2dea6012d9b8f4f8f5f0576d1a2462f7ee728997
sdgupta123 <sdgupta@akvagroup.com>
2020-09-24 19:19:59 +0200

    Integrating Signup page with confirmation

--- src/containers/Signup.js
+++ src/containers/Signup.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { useHistory } from "react-router-dom";
+import { Auth } from "aws-amplify";
 import {
   HelpBlock,
   FormGroup,
@@ -38,18 +39,33 @@ export default function Signup() {
 
   async function handleSubmit(event) {
     event.preventDefault();
-
     setIsLoading(true);
-
-    setNewUser("test");
-
-    setIsLoading(false);
+    try {
+      const newUser = await Auth.signUp({
+        username: fields.email,
+        password: fields.password
+      });
+      setIsLoading(false);
+      setNewUser(newUser);
+    } catch (e) {
+      onError(e);
+      setIsLoading(false);
+    }
   }
 
   async function handleConfirmationSubmit(event) {
     event.preventDefault();
-
     setIsLoading(true);
+    try {
+      await Auth.confirmSignUp(fields.email, fields.confirmationCode);
+      await Auth.signIn(fields.email, fields.password);
+
+      userHasAuthenticated(true);
+      history.push("/");
+    } catch (e) {
+      onError(e);
+      setIsLoading(false);
+    }
   }
 
   function renderConfirmationForm() {
