7e1519523129414f15ffccdb4950a14962a0752b
Das Gupta <showmen.das-gupta@capgemini.com>
2021-05-25 11:34:59 +0200

    Add api key

--- src/config.js
+++ src/config.js
@@ -57,6 +57,6 @@ const config = process.env.REACT_APP_STAGE === "prod" ? prod : dev;
 export default {
   // Add common config values here
   MAX_ATTACHMENT_SIZE: 5000000,
-  API_KEY: "AIzaSyBf53cJdRoYn5kZ-qBKC1ASIgP-_ibabtw",
+  API_KEY: "AIzaSyAwmLZGNV0uK_zTucxwyrWG2GDhTGAAIsQ",
   ...config
 };
